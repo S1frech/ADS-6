34b7482fcc222a18c53732be081359947561f7c4
Egoriy <91011699+S1frech@users.noreply.github.com>
2025-05-03 21:49:52 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,68 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
+template <typename T>
+struct Node {
+  T data;
+  Node<T>* next;
+  explicit Node(const T& data)
+      : data(data), next(nullptr) {}  
+};
+
+template <typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  Node<T>* head;
+  int size;
+
+ public:
+  TPQueue() : head(nullptr), size(0) {}
+  ~TPQueue();
+
+  void push(const T& value);
+  T pop();
+  bool isEmpty() const { return head == nullptr; }
+  int getSize() const { return size; }
 };
 
-struct SYM {
-  char ch;
-  int prior;
-};
+template <typename T>
+TPQueue<T>::~TPQueue() {
+  while (!isEmpty()) {
+    pop();
+  }
+}
 
-#endif  // INCLUDE_TPQUEUE_H_
+template <typename T>
+void TPQueue<T>::push(const T& value) {
+  Node<T>* newNode = new Node<T>(value);
+
+  if (isEmpty() || head->data.prior < value.prior) {
+    newNode->next = head;
+    head = newNode;
+  } else {
+    Node<T>* current = head;
+    while (current->next != nullptr &&
+           current->next->data.prior >= value.prior) {
+      current = current->next;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+  }
+  size++;
+}
+
+template <typename T>
+T TPQueue<T>::pop() {
+  if (isEmpty()) {
+    throw "Queue is empty";
+  }
+
+  Node<T>* temp = head;
+  T value = temp->data;
+  head = head->next;
+  delete temp;
+  size--;
+  return value;
+}
+
+#endif  // TPQUEUE_H
